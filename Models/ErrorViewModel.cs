e455751ae7ef41f5ff2b6b1fde77d87aa227c312
matheusvilar2019 <43919709+matheusvilar2019@users.noreply.github.com>
2026-05-03 19:29:59 -0300

    First commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVC_Azure_CICD_Teste.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
